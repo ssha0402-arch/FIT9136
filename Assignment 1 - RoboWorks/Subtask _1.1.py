0d7584dbaf87f689d1ebf84b988f75f5d284770b
ssha0402 <ssha0402@student.monash.edu>
2026-03-20 16:42:52 +0800

    fix subtask1.1

--- Assignment 1 - RoboWorks/Subtask _1.1.py
+++ Assignment 1 - RoboWorks/Subtask _1.1.py
@@ -1,5 +1,5 @@
 price = 52.99
 print ("Welcome to the motor line item test script.\n\nMotor price: $"+str(price))
 num = float(input("\nHow many motors would you like to buy: "))
-total = price * float(num)
+total = round(price * float(num)+0.001,2) #it seems like round() has some problem
 print ("Total price: $"+str(total))
--- Assignment 1 - RoboWorks/Subtask_2.1.py
+++ Assignment 1 - RoboWorks/Subtask_2.1.py
@@ -1,11 +1,14 @@
-part_name = ["motor", "sensor", "frame", "cpu"]
+# set origional list
+part_name  = ["motor", "sensor", "frame", "cpu"]
 part_stock = [5, 7, 10, 2]
-part_cost = [30.50, 10.20, 60.00, 45.95]
+part_cost  = [30.50, 10.20, 60.00, 45.95]
 
-if __name__ == "__main__":
-    i = 0
+#set counter
+i = 0 
 
-    while i < len(part_name):
-        price = part_cost[i] * part_stock[i]
-        print(part_name[i] + ": $" + str(f"{price:.2f}"))
-        i += 1
+#print name and price amount
+while i < len(part_name):
+    price = 0
+    price = part_cost[i] * part_stock[i]
+    print (part_name[i]+ ": $"+str(f"{price:.2f}"))
+    i += 1
